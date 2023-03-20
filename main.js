e9d958338be06d3bce6ffcf26f9d5b0323a3106f
saanvikhanna <87762768+saanvikhanna@users.noreply.github.com>
2023-03-20 15:38:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function draw()
+{
+    background('#57cc33');
+}
+
+function modelLoaded()
+{
+    console.log("poseNet mode is initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
